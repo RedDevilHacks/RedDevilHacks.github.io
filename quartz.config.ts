99dd4471eee2e8af72f4be2233f9f7ef1871b760
Ty Chermsirivatana <professornitro@gmail.com>
2025-03-03 11:37:31 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://reddevilhacks.github.io/RDHGarden/",
+    baseUrl: "reddevilhacks.github.io/RDHGarden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
